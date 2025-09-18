5891504685445f28fa28b209d687c5fcd36c5692
Shishura <murilotoledo2015@gmail.com>
2025-09-18 00:53:09 -0300

    feat:validador-cpf

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ print(f"Soma: {a} + {b} = {soma}")
 print(f"Subtração: {a} - {b} = {subtracao}")
 print(f"Multiplicação: {a} * {b} = {multiplicacao}")
 print(f"Divisão: {a} / {b} = {divisao}")
+
+
+cpf = "123.456.789-09"
+cpf_limpo = ''.join(filter(str.isdigit, cpf))
+cpf_valido = len(cpf_limpo) == 11 and not cpf_limpo == cpf_limpo[0] * 11
+print("CPF informado:", cpf)
+print("CPF limpo:", cpf_limpo)
+print("É válido?", "Sim" if cpf_valido else "Não")
