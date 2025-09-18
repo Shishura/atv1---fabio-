020c1ef4cea511af4aa963f967ad95e13cc96338
Shishura <murilotoledo2015@gmail.com>
2025-09-18 00:50:31 -0300

    feat:gerador-de-senha

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ print(f"Soma: {a} + {b} = {soma}")
 print(f"Subtração: {a} - {b} = {subtracao}")
 print(f"Multiplicação: {a} * {b} = {multiplicacao}")
 print(f"Divisão: {a} / {b} = {divisao}")
+
+
+
+print("\n=== 3. GERADOR DE SENHAS ===")
+tamanho_senha = 12
+caracteres = string.ascii_letters + string.digits + string.punctuation
+senha = ''.join(random.choice(caracteres) for _ in range(tamanho_senha))
+print("Senha gerada:", senha)
