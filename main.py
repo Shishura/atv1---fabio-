f57cd87b22305843a75d5628b30fd4fc0c2ae77e
Shishura <murilotoledo2015@gmail.com>
2025-09-18 00:40:29 -0300

    chore:init

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+print("\n=== 1. CALCULADORA ===")
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b if b != 0 else "Erro: divisão por zero"
